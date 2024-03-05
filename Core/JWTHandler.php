09ff1b0d4596c856f7dc0417b390ccf27fa1d9d1
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-03-05 01:34:07 -0400

    chore: Begin work on JWT token handling class

---
+++ Core/JWTHandler.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Core;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JWTHandler
+{
+
+  protected $issuedAt;
+  protected $expire;
+  private $payload;
+
+
+  public function __construct(array $params) {
+    date_default_timezone_set($_ENV['TIME_ZONE']);
+
+    $this->issuedAt = time();
+    $this->expire = $this->issuedAt + 10800;
+
+    $this->payload = [
+      'iss' => $_ENV['JWT_ISS'],
+      'aud'=> $_ENV['JWT_AUD'],
+      "customerId" => $params['customerID'],
+      "name" => $params['name'],
+      "admin" => $params['admin'],
+      "iat" => $this->issuedAt,
+      "exp" => $this->expire,
+      
+    ];
+  }
+
+  public static function encode(array $params){
+    $obj = new self($params);
+    // $payload_arr = $obj->payload;
+    return JWT::encode($obj->payload, $_ENV['JWT_KEY'], $_ENV['JWT_ALG']);
+  }
+
+  public static function decode($token){
+
+    try {
+
+      $decode = JWT::decode($token, new Key($_ENV['JWT_KEY'], $_ENV['JWT_ALG']));
+
+      return $decode->data;
+
+    } catch (\Exception $e) {
+      // $e;
+      Session::flush();
+
+      // Session::destroy();
+      // A key is needed.
+
+      Response::handler("UNAUTHORIZED");
+    }
+  }
+  
+}
