76a8a10971448405a6c6732264fd3cf3d67b2ccf
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-03-26 01:31:02 -0400

    chore: Remove theme meta information and add link to JS module

--- views/partials/head.php
+++ views/partials/head.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="color-scheme" content="light dark" />
+    <!-- <meta name="color-scheme" content="light dark" /> -->
     <title>Workout Logger</title>
     <!-- <meta name="description" content="A web for logging workout." /> -->
 
@@ -13,6 +13,7 @@
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
     />
+    <script type="module" src="js/main.js"></script>
   </head>
 
   <body>
