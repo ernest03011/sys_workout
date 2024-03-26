2fe7d42ae53cf634df558631b126a001a79a9267
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-03-26 01:01:55 -0400

    chore(routes): Add additional routes

--- routes.php
+++ routes.php
@@ -1,9 +1,19 @@
 <?php
 
-$router->get('/', "test.php");
+$router->get('/test', "test.php");
+$router->get('/', "workouts/dashboard.php");
 
 $router->get('/dashboard', "workouts/index.php");
 
 $router->get('/add', "workouts/create.php");
 $router->post('/add', "workouts/store.php");
+
+$router->get('/login', "sessions/create.php")->only('guest');
+$router->post('/login', "sessions/store.php")->only('guest');
+
+$router->post('/auth-token', "sessions/auth.token.php")->only('guest');
+
+$router->get("/workouts", "workouts/index.php")->only('auth');
+$router->get("/workout", "workouts/show.php")->only('auth');
+
 
