adadc785a88f3e6c559f27be6adacd841b23ba6d
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-04-01 00:20:31 -0400

    chore: Update parameters and presentation of each exercise when viewing a workout

--- Http/controller/workouts/show.php
+++ Http/controller/workouts/show.php
@@ -1,31 +1,18 @@
 <?php
 
 use Core\Database;
-use Http\controller\Curl\CurlController;
 
 // Show an specific workout based on the ID
 
 $workout_id = $_GET['id'];
 $db = new Database;
 
-$workout = $db->query('SELECT w.workout_name, w.date, e.exercise_name FROM workouts w JOIN workoutexercises we ON w.workout_id = we.workout_id
+$workouts = $db->query('SELECT w.workout_name, w.date, e.exercise_name, e.target, we.sets, we.reps, we.weight, we.duration, we.notes FROM workouts w JOIN workoutexercises we ON w.workout_id = we.workout_id
 JOIN exercises e ON we.exercise_id = e.exercise_id WHERE w.workout_id = :workout_id;
 ', [
   'workout_id' => $workout_id
-])->find();
-
-$url = 'https://exercisedb.p.rapidapi.com/';
-
-$exercise_name = replaceSpacesWithPercent20($workout['exercise_name']);
-$params = "exercises/name/{$exercise_name}";
-
-$ch = new CurlController($url, $params);
-$response_arr = $ch->execute();
-$ch->close();
-
-$data = json_decode($response_arr[0], true)[0];
+])->get();
  
 view('workouts/show.view.php', [
-  'workout' => $workout,
-  'exercise_data_from_api' => $data
-]);
+  'workouts' => $workouts
+]); 
--- views/workouts/show.view.php
+++ views/workouts/show.view.php
@@ -3,43 +3,46 @@
 
 <!-- // dd($workout); -->
 <div class="container mx-auto px-4">
-    <h2 class="text-2xl font-bold mb-4">Workout Sessions</h2>
+
+    <h2 class="text-2xl font-bold mb-4">Workout Session: <?= htmlspecialchars($workouts[0]['workout_name']); ?></h2>
+    <h3>Date: <?= htmlspecialchars($workouts[0]['date']); ?></h3>
+
     <div class="overflow-x-auto">
         <table class="table-auto border-collapse border border-gray-200">
             <thead>
+
                 <tr>
-                    <th class="px-4 py-2 bg-gray-200 border border-gray-200">Workout Name</th>
+
                     <th class="px-4 py-2 bg-gray-200 border border-gray-200">Exercise</th>
-                    <th class="px-4 py-2 bg-gray-200 border border-gray-200">Date</th>
-                    <th class="px-4 py-2 bg-gray-200 border border-gray-200">BodyPart</th>
-                    <th class="px-4 py-2 bg-gray-200 border border-gray-200">Equipment</th>
                     <th class="px-4 py-2 bg-gray-200 border border-gray-200">Target</th>
-                    <th class="px-4 py-2 bg-gray-200 border border-gray-200">Secondary Muscles</th>
-                    <th class="px-4 py-2 bg-gray-200 border border-gray-200">Instructions</th>
+                    <th class="px-4 py-2 bg-gray-200 border border-gray-200">Sets</th>
+                    <th class="px-4 py-2 bg-gray-200 border border-gray-200">Reps</th>
+                    <th class="px-4 py-2 bg-gray-200 border border-gray-200">Weight</th>
+                    <th class="px-4 py-2 bg-gray-200 border border-gray-200">Duration</th>
+                    <th class="px-4 py-2 bg-gray-200 border border-gray-200">Notes</th>
                 </tr>
+
             </thead>
             <tbody>
-                <tr>
-                    <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($workout['workout_name']); ?></td>
-                    <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($workout['exercise_name']); ?></td>
-                    <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($workout['date']); ?></td>
-                    <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($exercise_data_from_api['bodyPart']); ?></td>
-                    <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($exercise_data_from_api['equipment']); ?></td>
-                    <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($exercise_data_from_api['target']); ?></td>
-                    <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars(json_encode($exercise_data_from_api['secondaryMuscles'])); ?></td>
-                    <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars(json_encode($exercise_data_from_api['instructions'])); ?></td>
-                </tr>
+
+                <?php foreach($workouts as $workout) : ?>
+                    
+                    <tr>
+                        <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($workout['exercise_name']); ?></td>
+                        <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($workout['target']); ?></td>
+                        <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($workout['sets']); ?></td>
+                        <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($workout['reps']); ?></td>
+                        <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($workout['weight']); ?></td>
+                        <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($workout['duration']); ?></td>
+                        <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($workout['notes']); ?></td>
+                    </tr>
+           
+                <?php endforeach; ?>
+
             </tbody>
         </table>
     </div>
 
-    <div class="my-8">
-        <img src="<?= htmlspecialchars($exercise_data_from_api['gifUrl']); ?>" alt="">
-    </div>
 </div>
 
-<?php view("partials/footer.php"); ?>
-
-<!-- exercise_data_from_api -->
-
-<!-- {"bodyPart":"waist","equipment":"body weight","gifUrl":"https://v2.exercisedb.io/image/Hkfb84WY8VkL9D","id":"0006","name":"alternate heel touchers","target":"abs","secondaryMuscles":["obliques"],"instructions":["Lie flat on your back with your knees bent and feet flat on the ground.","Extend your arms straight out to the sides, parallel to the ground.","Engaging your abs, lift your shoulders off the ground and reach your right hand towards your right heel.","Return to the starting position and repeat on the left side, reaching your left hand towards your left heel.","Continue alternating sides for the desired number of repetitions."]} -->
+<?php view("partials/footer.php"); ?>   
