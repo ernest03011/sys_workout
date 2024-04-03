c6effa1970d04d87ac669167aab6b5c7b82428e4
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-04-02 22:39:45 -0400

    chore, add a note to explain how to get more details about the exercise and add links to the exercise name in order to take the user to the other page.

--- views/workouts/show.view.php
+++ views/workouts/show.view.php
@@ -6,7 +6,7 @@
 
     <h2 class="text-2xl font-bold mb-4">Workout Session: <?= htmlspecialchars($workouts[0]['workout_name']); ?></h2>
     <h3>Date: <?= htmlspecialchars($workouts[0]['date']); ?></h3>
-
+    <p class="my-4"><span class="font-bold">NOTE:</span> Click Exercise Name for more information about the exercise!</p>
     <div class="overflow-x-auto">
         <table class="table-auto border-collapse border border-gray-200">
             <thead>
@@ -28,7 +28,7 @@
                 <?php foreach($workouts as $workout) : ?>
                     
                     <tr>
-                        <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($workout['exercise_name']); ?></td>
+                        <td class="px-4 py-2 border border-gray-200"> <a class="font-semibold text-indigo-600" href="exercises?name=<?= htmlspecialchars($workout['exercise_name']); ?>"><?= htmlspecialchars($workout['exercise_name']); ?></a></td>
                         <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($workout['target']); ?></td>
                         <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($workout['sets']); ?></td>
                         <td class="px-4 py-2 border border-gray-200"><?= htmlspecialchars($workout['reps']); ?></td>
