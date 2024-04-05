4a95b5727b70d5abf30c8898f1a73bbd074509e3
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-04-05 01:33:56 -0400

    Add if statement to handle error or empty response from cURL connection, displaying an error message in those cases.

--- Http/controller/exercises/show.php
+++ Http/controller/exercises/show.php
@@ -22,13 +22,33 @@ try {
 
   $ch = new CurlController($url, $params);
   $response_arr = $ch->execute();
+  $err = $ch->getErrors();
+
   $ch->close();
 
-  $data = json_decode($response_arr[0], true)[0];
 
-  view("exercises/show.view.php", [
-    'exercise' => $data
-  ]);
+  if ($err) {
+
+    Router::redirect_with($prev_url, [
+      'error' => "Something went wrong while displaying the Exercise, try again!"
+    ]);
+    
+  } else {
+    $data = json_decode($response_arr[0], true)[0];
+    
+    if(! $data){      
+      Router::redirect_with($prev_url, [
+        'error' => "Something went wrong while displaying the Exercise, try again!"
+      ]);
+    }else{
+      view("exercises/show.view.php", [
+        'exercise' => $data
+      ]);
+    }
+  
+  }
+
+
 
 } catch (\Exception $e) {
   Router::redirect_with($prev_url, [
