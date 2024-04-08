18a39ca1f88c2cee789142018b7b2189e639908d
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-04-07 23:28:38 -0400

    chore: Implement functionality to switch between hamburger and x button depending on whether the mobile menu is expanded or collapsed.

--- public/js/main.js
+++ public/js/main.js
@@ -198,13 +198,31 @@ function toggleMobileMenu() {
   // console.log(mobileMenuButton.ariaExpanded);
 
   const mobileMenu = document.getElementById("mobile-menu");
+  const openMobileMenuBtn = mobileMenuButton.querySelector(
+    "#open-mobile-menu-btn"
+  );
+  const closeMobileMenuBtn = mobileMenuButton.querySelector(
+    "#close-mobile-menu-btn"
+  );
 
   if (isExpanded == "false") {
     mobileMenu.classList.remove("md:hidden");
     mobileMenu.classList.add("hidden");
+
+    openMobileMenuBtn.classList.remove("hidden");
+    openMobileMenuBtn.classList.add("block");
+
+    closeMobileMenuBtn.classList.remove("block");
+    closeMobileMenuBtn.classList.add("hidden");
   } else if (isExpanded == "true") {
     mobileMenu.classList.remove("hidden");
     mobileMenu.classList.add("md:hidden");
+
+    openMobileMenuBtn.classList.remove("block");
+    openMobileMenuBtn.classList.add("hidden");
+
+    closeMobileMenuBtn.classList.remove("hidden");
+    closeMobileMenuBtn.classList.add("block");
   }
 
   mobileMenuButton.ariaExpanded = isExpanded == "true" ? "false" : "true";
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -29,11 +29,11 @@
             <span class="sr-only">Open main menu</span>
 
             <!-- Menu open: "hidden", Menu closed: "block" -->
-            <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <svg id="open-mobile-menu-btn" class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
             <!-- Menu open: "block", Menu closed: "hidden" -->
-            <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <svg id="close-mobile-menu-btn" class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
