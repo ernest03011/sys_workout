ef3e025baad2d3189f2b0e4e2d4c42a94046403d
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-04-09 23:03:41 -0400

    chore: Remove the redirect link

--- views/sessions/create.view.php
+++ views/sessions/create.view.php
@@ -44,7 +44,6 @@
     </div>
     <section class="flex items-center justify-between">
         <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-700 focus:outline-none focus:bg-blue-700">Login</button>
-        <a href="register" class="text-blue-500 hover:underline">Register</a>
     </section>
 </form>
 
