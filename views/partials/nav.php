29093aedf892c772601ef6b4a52a60349b8f4ed3
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-04-11 02:27:29 -0400

    chore: Update both desktop and mobile menus to highlight the current page based on the URI

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -11,9 +11,9 @@
 
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-              <a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-              <a href="/workouts" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Workouts</a>
-              <a href="/logout" class=" text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-mediu">Logout</a>
+              <a href="/" class="<?= getCurrentURI() == '/' ? 'bg-gray-900' : 'hover:bg-gray-700 hover:text-white'?>  text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
+              <a href="/workouts" class="<?= getCurrentURI() == '/workouts' ? 'bg-gray-900' : 'hover:bg-gray-700 hover:text-white'?> text-gray-300 rounded-md px-3 py-2 text-sm font-medium">Workouts</a>
+              <a href="/logout" class=" <?= getCurrentURI() == '/logout' ? 'bg-gray-900' : 'hover:bg-gray-700 hover:text-white'?> text-gray-300 block rounded-md px-3 py-2 text-base font-mediu">Logout</a>
 
             </div>
 
@@ -46,9 +46,9 @@
     <div class="hidden" id="mobile-menu">
       <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-        <a href="/" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
-        <a href="/workouts" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Workouts</a>
-        <a href="/logout" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Logout</a>
+        <a href="/" class="<?= getCurrentURI() == '/' ? 'bg-gray-900' : 'hover:bg-gray-700 hover:text-white'?>  text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Home</a>
+        <a href="/workouts" class="<?= getCurrentURI() == '/workouts' ? 'bg-gray-900' : 'hover:bg-gray-700 hover:text-white'?> text-gray-300 block rounded-md px-3 py-2 text-base font-medium">Workouts</a>
+        <a href="/logout" class="<?= getCurrentURI() == '/logout' ? 'bg-gray-900' : 'hover:bg-gray-700 hover:text-white'?> text-gray-300 block rounded-md px-3 py-2 text-base font-medium">Logout</a>
 
 
       </div>
