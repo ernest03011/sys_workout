55fcf9b55cfe65191300744a1e8e6bc520f7303d
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-04-21 23:49:41 -0400

    chore: Add some top margin to the entire content

--- views/401.php
+++ views/401.php
@@ -1,6 +1,6 @@
 <?php view("partials/head.php"); ?>
 
-    <div class="text-center">
+    <div class="text-center mt-14">
         <p class="text-base font-semibold text-indigo-600">401</p>
         <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Unauthorized</h1>
         <p class="mt-6 text-base leading-7 text-gray-600">Sorry, you are not authorized to access this resource.</p>
