abb6fa398e278e079fcba294f601bf259698d4d3
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2024-04-23 00:39:42 -0400

    chore: Handle issues when there are no stats for an exercise yet and display an error message for those cases

--- Http/controller/stats/show.php
+++ Http/controller/stats/show.php
@@ -1,5 +1,6 @@
 <?php
 
+use Core\Router;
 use Core\Database;
 
 $db = new Database;
@@ -9,6 +10,14 @@ $exercise_name = $_GET['name'];
 $data = $db->query('SELECT e.exercise_name, we.sets, we.reps, we.weight, we.duration, w.date from exercises e JOIN workoutexercises we ON e.exercise_id = we.exercise_id JOIN workouts w ON we.workout_id = w.workout_id WHERE e.exercise_name = :exercise_name', [
   'exercise_name' => $exercise_name
 ])->get();
+
+
+if(empty($data)){
+  $prevUrl = Router::previousUrl();
+  Router::redirect_with($prevUrl, [
+    "error" => "There are no stats for this exercise yet!"
+  ]);
+}
 
 view('stats/show.view.php', [
   'data' => $data
